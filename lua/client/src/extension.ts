9383167ad15d6048ffa3e17b38b0a0b281cb4233
Wiktor Adamski <wiktor.marek.adamski@gmail.com>
2018-01-07 13:02:44 +0100

    added chapter about client side of LSP, small corrections li bibliography

--- lua/client/src/extension.ts
+++ lua/client/src/extension.ts
@@ -1,7 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 'use strict';
 
 import * as path from 'path';
@@ -11,34 +7,29 @@ import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } f
 
 export function activate(context: ExtensionContext) {
 
-	// The server is implemented in node
+	// lokalizacja modułu Node.js serwera
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
-	// The debug options for the server
+	// ustawienia umożliwiające debugowanie kodu serwera
 	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 	
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
+	// ustawienia serwera
 	let serverOptions: ServerOptions = {
 		run : { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
 	}
 	
-	// Options to control the language client
+	// opcje klienta
 	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
 		documentSelector: [{scheme: 'file', language: 'lua'}],
 		synchronize: {
-			// Synchronize the setting section 'languageServerExample' to the server
 			configurationSection: 'lua-lang',
-			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
 	
-	// Create the language client and start the client.
-	let disposable = new LanguageClient('lspSample', 'Language Server Example', serverOptions, clientOptions).start();
+	// Uruchomienie klienta LSP
+	let client = new LanguageClient('luaLanguage', 'Lua language extension', serverOptions, clientOptions).start();
 	
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
-	context.subscriptions.push(disposable);
+	// Dodanie klienta do kolekcji elementów usuwanych przy zamknięciu rozszerzenia
+	context.subscriptions.push(client);
 }
